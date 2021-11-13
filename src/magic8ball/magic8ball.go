61da4b313e3f893b0d007f8a192588c48206e08a
Gergely Nemeth <86304644+GregNemeth@users.noreply.github.com>
2021-11-13 15:47:18 +0000

    Update magic8ball.go

--- src/magic8ball/magic8ball.go
+++ src/magic8ball/magic8ball.go
@@ -1,22 +1,23 @@
 package magic8ball
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+        "fmt"
+        "math/rand"
+        "time"
+        "net/http"
 )
 
-func magic8ball() {
-	rand.Seed(time.Now().UnixNano())
-	answers := [8]string{
-		"Not today!",
-		"Perhaps",
-		"I'm not sure about that",
-		"Try again later!",
-		"Today is definetely the day for that",
-		"I'm really not feeling it!",
-		"Yes, definetely",
-		"Sure!",
-	}
-	fmt.Println("Magic8ball says:", answers[rand.Intn(len(answers))])
+func magic8ball(w http.ResponseWriter, r *http.Request) {
+        rand.Seed(time.Now().UnixNano())
+        answers := [8]string{
+                "Not today!",
+                "Perhaps",
+                "I'm not sure about that",
+                "Try again later!",
+                "Today is definetely the day for that",
+                "I'm really not feeling it!",
+                "Yes, definetely",
+                "Sure!",
+        }
+        fmt.Fprintln(w, "Magic8ball says:", answers[rand.Intn(len(answers))])
 }
